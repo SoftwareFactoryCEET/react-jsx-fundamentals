f15fef1dd952d32e9d25268332edaab4d05a5da3
LAPTOP01\vladi <vccortesa@eafit.edu.co>
2024-02-26 21:04:57 -0500

    feat(front-end): Add 08-actualizando-la-pantalla

---
+++ 08-actualizando-la-pantalla/src/App.jsx
@@ -0,0 +1,15 @@
+import MyButton from "./components/MyButton.jsx";
+
+function App(){
+    return(
+        <>
+            <h2>Counters that update separately</h2>
+            <MyButton/>
+
+            <MyButton/>
+
+            <MyButton/>
+        </>
+    );
+}
+export default App
